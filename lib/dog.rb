0e75dc85d63cb6e333f785237d4d1d7efe196363
Dominic Chapman <dominicchapman@me.com>
2017-09-28 19:20:31 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+
+class Dog
+  def initialize(name)
+    # instance variable, prefaced with @
+    @name = name
+  end
+
+  # we can call the name we have initialized with a name method
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+end
